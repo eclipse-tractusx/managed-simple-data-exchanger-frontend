3d8262c23c050762388340a8b6bdff704d9ce2ce
adityagajbhiye <adityagajbhiye1@gmail.com>
2022-09-08 19:29:38 +0530

    Update env-config.js

--- public/env-config.js
+++ public/env-config.js
@@ -1,8 +1,8 @@
 window._env_ = {
-  REACT_APP_FILESIZE: "268435456",
-  REACT_APP_API_URL: "https://dft-api.int.demo.catena-x.net/api",
-  REACT_APP_DEFAULT_COMPANY_BPN: "BPNL00000005PROV",
-  REACT_APP_CLIENT_ID: "Cl15-CX-DFT",
-  REACT_APP_CLIENT_REALM: "CX-Central",
+  REACT_APP_FILESIZE: '268435456',
+  REACT_APP_API_URL: 'https://dft-api.int.demo.catena-x.net/api',
+  REACT_APP_DEFAULT_COMPANY_BPN: 'BPNL00000005PROV',
+  REACT_APP_CLIENT_ID: 'Cl15-CX-DFT',
+  REACT_APP_CLIENT_REALM: 'CX-Central',
   REACT_APP_KEYCLOAK_URL: 'https://centralidp.demo.catena-x.net/auth'
 };
