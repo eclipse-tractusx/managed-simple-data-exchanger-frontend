9880f57a5b3691f31dcdc1bc639299c3abb45b25
Ajith Kumar <103632877+ajithsimons@users.noreply.github.com>
2022-11-16 16:15:22 +0530

    new keycloak url updated

--- public/env-config.js
+++ public/env-config.js
@@ -3,7 +3,7 @@ window._env_ = {
   REACT_APP_API_URL: 'https://dft-api.int.demo.catena-x.net/api',
   REACT_APP_DEFAULT_COMPANY_BPN: 'BPNL0000000007UH',
   REACT_APP_CLIENT_ID: 'app68',
-  REACT_APP_KEYCLOAK_URL: 'https://centralidp.demo.catena-x.net/auth',
+  REACT_APP_KEYCLOAK_URL: 'https://centralidp.int.demo.catena-x.net/auth',
   REACT_APP_KEYCLOAK_REALM: 'CX-Central'
 };
 
