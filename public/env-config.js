2087b0a1491dfcf7cb71a4e2f6c13ad8d439a576
adityagajbhiye9 <133367448+adityagajbhiye9@users.noreply.github.com>
2023-09-06 14:41:42 +0530

    image

--- public/env-config.js
+++ public/env-config.js
@@ -20,10 +20,11 @@
 
 window._env_ = {
   REACT_APP_FILESIZE: '268435456',
-  REACT_APP_API_URL: '',
-  REACT_APP_DEFAULT_COMPANY_BPN: '',
-  REACT_APP_CLIENT_ID: '',
-  REACT_APP_KEYCLOAK_URL: '',
-  REACT_APP_KEYCLOAK_REALM: '',
+  REACT_APP_API_URL: 'https://dft-api.int.demo.catena-x.net/api',
+  REACT_APP_DEFAULT_COMPANY_BPN: 'BPNL001000TS0100',
+  REACT_APP_CLIENT_ID: 'Cl15-CX-DFT',
+  REACT_APP_KEYCLOAK_URL: 'https://centralidp.int.demo.catena-x.net/auth',
+  REACT_APP_KEYCLOAK_REALM: 'CX-Central',
 };
+
 
