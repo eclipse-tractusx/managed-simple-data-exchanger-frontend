04428e4ce5e214b41c298a096509e033ce00f4ae
ajithkumar.s <ajithkumar.s@t-systems.com>
2024-03-19 17:18:31 +0530

    missing constants added

---
+++ src/constants/policies.ts
@@ -0,0 +1,27 @@
+/********************************************************************************
+ * Copyright (c) 2024 T-Systems International GmbH
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
+const SELECT_POLICY_TYPES = ['Brands', 'Version', 'Static', 'DynamicValue'];
+
+const ADD_POLICY_DIALOG_TYPES = ['Add', 'FileWithPolicy', 'TableWithPolicy'];
+
+const NEW_POLICY_ITEM = { id: 0, title: 'Create New Policy', value: 'NEW' };
+
+export { ADD_POLICY_DIALOG_TYPES, NEW_POLICY_ITEM, SELECT_POLICY_TYPES };
